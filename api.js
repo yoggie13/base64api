3f26d7bf9366dc2c8cd853183c29d63fce0d7f7f
Ognjen <ognjennikolic13@gmail.com>
2023-03-30 12:09:29 +0200

    netlify needs

--- api.js
+++ api.js
@@ -1,9 +1,12 @@
 const PORT = process.env.PORT || 3001;
 
 const express = require("express");
+const serverless = require("serverless-http");
 const fetch = require("node-fetch");
 const imageToBase64 = require("image-to-base64");
+
 const app = express();
+const router = express.Router();
 const cors = require("cors");
 
 app.options("/", cors());
@@ -11,9 +14,14 @@ app.use(cors());
 
 app.listen();
 
-app.get("/", async (req, res) => {
+router.get("/", async (req, res) => {
   var data = await fetch(req.query.url);
   var b = await data.blob();
   var response = await imageToBase64(req.query.url);
   res.send("data:" + b.type + ";base64," + response);
 });
+
+app.use(`/.netlify/functions/api`, router);
+
+module.exports = app;
+module.exports.handler = serverless(app);
