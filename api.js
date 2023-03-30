6422a2454a5fb86524e4ffed4fc69a74f1722b30
Ognjen <ognjennikolic13@gmail.com>
2023-03-30 12:19:37 +0200

    deleted netlify

--- api.js
+++ api.js
@@ -1,12 +1,10 @@
 // const PORT = process.env.PORT || 3001;
 
 const express = require("express");
-const serverless = require("serverless-http");
 const fetch = require("node-fetch");
 const imageToBase64 = require("image-to-base64");
 
 const app = express();
-const router = express.Router();
 const cors = require("cors");
 
 app.options("/", cors());
@@ -14,14 +12,9 @@ app.use(cors());
 
 app.listen();
 
-router.get("/", async (req, res) => {
+app.get("/", async (req, res) => {
   var data = await fetch(req.query.url);
   var b = await data.blob();
   var response = await imageToBase64(req.query.url);
   res.send("data:" + b.type + ";base64," + response);
 });
-
-app.use(`/.netlify/functions/api`, router);
-
-module.exports = app;
-module.exports.handler = serverless(app);
